832c6e99a5547b5a07ac53135ff89b2d7fb1736b
ofaaoficial <oscarfamado@gmail.com>
2019-11-19 20:46:49 -0500

    create model for support translations and basic config of library laravel-translatable

--- app/Post.php
+++ app/Post.php
@@ -3,13 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
 class Post extends Model
 {
+    use \Astrotomic\Translatable\Translatable;
+
     protected $table = 'posts';
 
     protected $fillable = [
         'slug',
         'user_id',
+    ];
+
+    public $translatedAttributes = [
+        'title',
+        'description'
     ];
 }
